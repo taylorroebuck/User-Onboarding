b3cd12c92e0511c092ad9c79cff6d1fe1ba4c91c
Taylor Roebuck <taylornroebuck@gmail.com>
2020-02-20 19:44:45 -0700

    initial commit

---
+++ formik-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+function App() {
+  return (
+    <div className="App">
+      User onboarding
+    </div>
+  );
+}
+
+export default App;
