6d12c198914a7d5bdfaa85a1ceb212c2d2c30578
Taylor Roebuck <taylornroebuck@gmail.com>
2020-02-24 19:47:26 -0700

    MVP with stretch

--- formik-app/src/App.js
+++ formik-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Formik } from 'formik';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div className="App">
       User onboarding
+      <Form />
     </div>
   );
 }
